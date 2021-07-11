149a908e01f2142d289d5fb0b7fd9f11540ba0e9
billyATGE <46874350+billyATGE@users.noreply.github.com>
2021-07-10 21:02:28 -0400

    Add files via upload

--- stock_predictions/app.py
+++ stock_predictions/app.py
@@ -1,5 +1,9 @@
 # import necessary libraries
 import os
+import pandas as pd 
+from sqlalchemy import create_engine, Column, Integer, String, Float, inspect
+from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 from flask import (
     Flask,
     render_template,
@@ -17,7 +21,7 @@ app = Flask(__name__)
 #################################################
 
 # from flask_sqlalchemy import SQLAlchemy
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///../stock.sqlite"
 
 # # Remove tracking modifications
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -49,20 +53,14 @@ def home():
 #     return render_template("form.html")
 
 
-# @app.route("/api/pals")
-# def pals():
-#     results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
-
+# @app.route("/api/pals")StaticPool
 #     hover_text = [result[0] for result in results]
 #     lat = [result[1] for result in results]
 #     lon = [result[2] for result in results]
 
 #     pet_data = [{
 #         "type": "scattergeo",
-#         "locationmode": "USA-states",
-#         "lat": lat,
-#         "lon": lon,
-#         "text": hover_text,
+#         "locationmode": "USA-states",results
 #         "hoverinfo": "text",
 #         "marker": {
 #             "size": 15,
@@ -74,7 +72,25 @@ def home():
 #     }]
 
 #     return jsonify(pet_data)
-
+@app.route("/api/db")
+def localdb():
+    engine = create_engine('sqlite:///stock.db', connect_args={'check_same_thread': False}, poolclass = StaticPool)
+    inspector = inspect(engine)
+    results = inspector.get_table_names()
+    session = Session(bind=engine)
+    session.commit()
+    session.close()
+    return render_template('stocks.html', results=results)
+@app.route("/api/stock_table/<stock>", methods=("POST", "GET"))
+def localstock(stock):
+    engine = create_engine('sqlite:///stock.db', connect_args={'check_same_thread': False}, poolclass = StaticPool).connect()
+    session = Session(bind=engine)
+    df = pd.read_sql_table(stock, engine)
+    session.commit()
+    session.close()
+    df.sort_values(by=['index'], inplace=True, ascending=False)
+    clean_df = df.head(10)
+    return render_template('stocks_detail.html', tables=[clean_df.to_html(classes='table table-striped', index=False, table_id = "data")], titles=clean_df.columns.values)
 
 if __name__ == "__main__":
     app.run()
